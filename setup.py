7faebf97bf108bf402f2ef6ee34d190e10fe06b8
Sebastian Kosch <sebastian@aldusleaf.org>
2020-11-16 17:52:52 -0800

    packing attempt 2

--- setup.py
+++ setup.py
@@ -2,21 +2,27 @@
 
 from distutils.core import setup, Extension
 import numpy as np
-import pathlib
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
+def parse_requirements(filename):
+        """Load requirements from a pip requirements file."""
+        lineiter = (line.strip() for line in open(filename))
+        return [line for line in lineiter if line and not line.startswith("#")]
 
 c_adt = Extension('adt', sources = ['anisotropic-distance-transform/ext/adt.c'], include_dirs = [np.get_include()])
 
 setup(name = 'anisotropic-distance-transform',
       version = '0.1',
       description = 'Anisotropic Euclidean Distance Transform (2D)',
-      long_description=README,
-      ext_modules = [c_adt],
+      long_description=long_description,
       long_description_content_type="text/markdown",
+      ext_modules = [c_adt],
+      author="Sebastian Kosch",
+      author_email="skosch@users.noreply.github.com",
       url="https://github.com/skosch/anisotropic-distance-transform",
-      author="Jan Hosang, Sebastian Kosch",
       license="MIT",
       classifiers=[
           "License :: OSI Approved :: MIT License",
@@ -24,6 +30,6 @@ setup(name = 'anisotropic-distance-transform',
           "Programming Language :: Python :: 3.7",
       ],
       packages=["anisotropic-distance-transform"],
-      include_package_data=True,
-      install_requires=["numpy"],
-      extras_require={"dev": ["matplotlib"]})
+      #install_requires=parse_requirements("requirements.txt"),
+      #extras_require={"dev": ["matplotlib"]}
+)
