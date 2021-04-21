cd019e86ffb3413e1a04eaa89556efbbdf0bdaf5
Sebastian Kosch <sebastian@aldusleaf.org>
2021-04-20 20:17:54 -0700

    improve readme and add sample image

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,27 @@
-
 import numpy as np
-
-import C_adt
-
 import matplotlib.pyplot as plt
 
-# Create a random image
+import adt
 
-orig_img = np.zeros((100, 200)).astype(np.float32)
-maxval = (np.sqrt(2)*200) ** 2 * 3 * 2
-orig_img[40:60, 40:60] = 1.
+# Create an image (here, just a square in the middle of zeros)
+orig_img = np.zeros((200, 200)).astype(np.float32)
+orig_img[90:110, 90:110] = 1.
 
-adt_img = maxval - maxval * orig_img
-C_adt.adt(adt_img, 2., 3., 4, 5.)
-#C_adt.adt(adt_img, 1, 1, 1, 1)
-plt.imshow(orig_img, cmap='gray')
-plt.imshow(adt_img, alpha=0.2, cmap='gray')
-plt.imshow(adt_img, alpha=0.2, cmap='inferno')
-plt.contour(adt_img, 20)
+# Define a maximum value (see the marching parabolas paper)
+maxval = (np.sqrt(2)*200) ** 2 * 10 * 2
+
+# Set everything to the maxval, except for the square (which becomes zero) ...
+adt_img_1 = maxval - maxval * orig_img
+# ... then apply the distance transform in place, like so
+adt.adt(adt_img_1, 1, 1, 1, 1)
+
+adt_img_2 = maxval - maxval * orig_img
+# ... and, optionally, set factors for values for left, right, top, and bottom
+adt.adt(adt_img_2, .5, 2., 1., 4.)
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(adt_img_1, cmap='gist_rainbow')
+ax[0].contour(orig_img, cmap='gray')
+ax[1].imshow(adt_img_2, cmap='gist_rainbow')
+ax[1].contour(orig_img, cmap='gray')
 plt.show()
